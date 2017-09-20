b50fe946b10ed2cc5bca8c55219869eb48313afa
Etienne FICHOT <fichotetienne@gmail.com>
2017-09-20 18:51:34 +0200

    blockchain functional, need to config websocket

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var WebSocket = require("ws");
 var express = require("express");
 var bodyParser = require("body-parser");
+var CryptoJS = require('crypto-js');
 
 var http_port = process.env.HTTP_PORT || 3001;
 var p2p_port = process.env.P2P_PORT || 6001;
@@ -34,13 +35,53 @@ var connectToPeers = (newPeers) => {
   });
 }
 
+var getLatestBlock = () => blockchain[blockchain.length - 1];
+
+var calculateHash = (index, previousHash, timestamp, data) => CryptoJS.SHA256(index + previousHash + timestamp + data).toString(); // hash block
+
+var generateNextBlock = (data) => {
+  var previousBlock = getLatestBlock();
+  var nextIndex = previousBlock.index + 1;
+  var nextTimestamp = Date.now();
+  var nextHash = calculateHash(nextIndex, previousBlock.hash, nextTimestamp, data);
+  return new Block(nextIndex, previousBlock.hash, nextTimestamp, data, nextHash);
+}
+
+var calculateHashForBlock = (block) => calculateHash(block.index, block.previousHash, block.timestamp, block.data);
+
+var isValidNewBlock = (newBlock, previousBlock) => {
+  if (previousBlock.index + 1 !== newBlock.index) {
+    console.log('invalid index');
+    return false;
+  } else if (previousBlock.hash !== newBlock.previousHash) {
+    console.log('invalid previous hash');
+    return false;
+  } else if (calculateHashForBlock(newBlock) !== newBlock.hash) {
+    console.log(typeof calculateHashForBlock(newBlock));
+    console.log(typeof newBlock.hash);
+    console.log('invalid hash block');
+    return false;
+  }
+  return true;
+}
+
+var addBlock = (newBlock) => {
+  if (isValidNewBlock(newBlock, getLatestBlock())) {
+    blockchain.push(newBlock);
+    console.log('block added: ' + JSON.stringify(newBlock));
+  }
+}
+
 var initHttpServer = () => {
   var app = express();
   app.use(bodyParser.json());
 
   app.get('/blocks', (req, res) => res.send(JSON.stringify(blockchain))); //route for print the block corresponding to http port
-  app.post('/mineBlock', (req, res) => { // mine block with data in it
-
+  app.post('/mineBlock', (req, res) => { // data for the new block in the POST req 
+    var newBlock = generateNextBlock(req.body.data); //create block
+    addBlock(newBlock); // check if the block is valid and add block in the blockchain
+    
+    res.send();
   });
 
   app.listen(http_port, () => console.log('Listening http on port: ' + http_port));
